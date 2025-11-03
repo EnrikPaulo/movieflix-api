6685251e97cd43046b71aa9a921ad15c75e25915
EnrikPaulo <enrikp.2016@gmail.com>
2025-11-03 18:46:52 -0300

    Adicionado swagger com cadrastro de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { PrismaClient } from "@prisma/client";
-import { log } from "console";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" with { type: "json" };
 
 
 const port = 3000;
@@ -8,6 +9,7 @@ const app = express()
 const prisma = new PrismaClient()
 
 app.use(express.json())
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/movies", async (_, res) => {
     try {
